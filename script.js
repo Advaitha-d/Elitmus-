2f11faf797398c0f1bdf2dc02be6c2b869a882c2
Advaitha-d <108218457+Advaitha-d@users.noreply.github.com>
2023-04-24 00:01:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+   
+    function handleImageClick() {
+      alert('woahh! to find your next clue, locate an injecting instrument.');
+      
+      window.location.href = "index4.html";
+      // Add your desired code here for handling the click event on the image portion
+    }
+    
+    function handleImageClick2() {
+      alert('Great! heading to your next clue.');
+      alert('I can be used to block the sun but am not the sunscreen, am worn on the head but am not a pair of sunglasses.');
+     
+      window.location.href = "index4.html";
+      // Add your desired code here for handling the click event on the image portion
+    }
+    function handleImageClick3() {
+      
+      alert('Bravo!! Find the next clue, "Am used to cut things, but am not a pair of scissors."');
+      alert('Remember. According to a witness, the killer wears a cap');
+      window.location.href = "index4.html";
+      // Add your desired code here for handling the click event on the image portion
+    }
+    function handleImageClick4() {
+      alert('Pay attention to the last clue it plays a key role.');
+      alert('Find a torn blue fabric.');
+      window.location.href = "index4.html";
+      // Add your desired code here for handling the click event on the image portion
+    }
+    function handleImageClick5() {
+      alert('This fabric is covered in blood; Lets piece it back together and see what it looks like.');
+     
+      window.location.href = "index5.html";
+      // Add your desired code here for handling the click event on the image portion
+    }
+    
+       
---
+++ script2.js
@@ -0,0 +1,15 @@
+
+    
+    function handleImageClick6() {
+        alert("Good job officer! You did well on this case!");
+        alert('"MISSION SUCCESSFUL."');
+        window.location.href = "index6.html";
+        // Add your desired code here for handling the click event on the image portion
+      }
+      function handleImageClick7() {
+        alert('Unfortunately, you are wrong.');
+       alert('Better luck next time.')
+        window.location.href = "index4.html";
+        // Add your desired code here for handling the click event on the image portion
+      }
+        
